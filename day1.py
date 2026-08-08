2956a68559692ef43f7bd730879d53f6c1b88d3b
Your Name <your@email.com>
2026-08-08 19:27:32 +0530

    Day 1 and Day 2: Python practice

---
+++ day1.py
@@ -0,0 +1,64 @@
+print("============================")
+print(           "Student"               )
+print("============================")
+print('''Name:Priya
+Age:22
+         ''')
+print("============================")
+print("A","D","G",sep="/")
+print("A",end=" ")
+print("B")
+a=10
+b=20
+print(a,b)
+print(a+b)
+
+#print box
+print("##########################")
+print('''
+      #                           #
+      #         Python            #
+      #                           #
+      ''')
+print("###########################")
+
+
+
+print("*************")
+print("*           *")
+print("*           *")
+print("*  python   *")
+print("*   Day-1   *")
+print("*           *")
+print("*           *")
+print("*************")          
+
+#using F string
+a=("#" *10)
+b=("Python")
+c=("day1")
+print(f"{a}")
+print(f"#{'':5}#")
+print(f"#{b:^5}#")
+print(f"#{c:^5}#")
+print(f"#{'':^5}#")
+print(f"{a}")
+
+
+border=("=" * 20)
+name=("Priya")
+age=(22)
+city=("ranipet")
+print(f"{border}")
+print(f"|{'':18}|")
+print(f"|{'':18}|")
+print(f"|{name:^18}|")
+print(f"|{age:^18}|")
+print(f"|{city:^18}|")
+print(f"|{'':18}|")
+
+
+
+
+
+print(f"{border}")
---
+++ day2.py
@@ -0,0 +1,84 @@
+# name="priya"
+# age=22
+# city="ranipet"
+# course="python"
+# mark=95
+# border="="*30
+
+# print("===================")
+# print("Student Details"    )
+# print("===================")
+# print(f"Name:{name}")
+# print(f"age:{age}")
+# print(f"Cty:{city}")
+# print(f"Course:{course}")
+# print(f"Mark:{mark}")
+# print("===================")
+
+# id= 20
+# name= "priya"
+# role= "python developer"
+# salary= 50000
+# experience= 2
+
+# print("*************************")
+# print("Employee Details")
+# print("*************************")
+
+# print(f"{'ID':<11}: {id}")
+# print(f"{'Name':<11}: {name}")
+# print(f"{'Role':<11}: {role}")
+# print(f"{'Salary':<11}: {salary}")
+# print(f"{'Experience':<11}: {experience}")
+# print("*************************")
+
+
+# print(input("Enter Your name:"))
+# print(input("Enter Your age:"))
+
+# print("---------STUDENT INFO------")
+# print(f"Name: {name}")
+# print(f"Age: {age}")
+# print("---------------------------")
+
+
+
+
+#user input 
+# print("Student info")
+# Name=input("Enter your name:")
+# Age=int(input("Enter your age:"))
+# Course=input("Enter your course:")
+
+
+# age = int(input("Enter age: "))
+# print(age + 1)
+
+# #simple calculator
+# a=10
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+
+# age = str(25)#sting to integer
+# print(age)
+
+#if statement
+if(10 == 5):
+    print("yes")
+else:
+    print("no")
+
+marks = 40
+print(marks < 35)
+
+
+
+
+
+
+
+
+
